3f3895935e9e624025d9041041b9ed1c6a7c2a05
unknown <macjay314@gmail.com>
2020-07-22 09:28:16 -0700

    Initial commit.

---
+++ travel-client-redux/src/__tests__/reducers/places-reducer.test.js
@@ -0,0 +1,9 @@
+import placesReducer from '../../reducers/places-reducer';
+
+describe ('placesReducer', () => {
+  const defaultState = {
+    isLoading: false,
+    places: [],
+    error: null
+  };
+}
