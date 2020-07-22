be5c17980b35bddd5d950f0f64e764d9078276bc
unknown <macjay314@gmail.com>
2020-07-22 10:05:03 -0700

    Retrieve bearer token from api.

--- travel-client-redux/src/__tests__/reducers/places-reducer.test.js
+++ travel-client-redux/src/__tests__/reducers/places-reducer.test.js
@@ -6,4 +6,14 @@ describe ('placesReducer', () => {
     places: [],
     error: null
   };
-}
+
+  test('should successfully return the default state if no action is passed into it', () => {
+    expect(placesReducer(defaultState, {type: null })).toEqual(
+      {
+        isLoading: false,
+        places: [],
+        error: null
+      }
+    );
+  });
+});
